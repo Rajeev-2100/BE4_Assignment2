f35210ae7f92d8d62da56591c226da29cf100498
Rajeev Rawat <rajeevrawat2100@gmail.com>
2025-11-25 14:21:32 +0530

    feat: add POST /recipes API to create new recipe with proper error handling

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const Recipe = require('./model/recipe.model.js')
+const fs = require('fs')
+
+const { intailizeDatabase } = require('./db/db.connect.js')
+
+intailizeDatabase()
+
+
+async function createNewRecipe(newRecipe){
+    try {
+        const recipe = new Recipe(newRecipe)
+        const savedRecipe = await recipe.save()
+        return savedRecipe
+    } catch (error) {
+        throw error
+    }
+}
+
+app.post('/recipes', async (req, res) => {
+    try {
+        const recipe = await createNewRecipe(req.body)
+        res.json(recipe)
+    } catch (error) {
+        res.status(500).json({error: 'Failed to Recipe detail'})
+    }
+})
+
+
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log('Server is running on this ', PORT)
+})
