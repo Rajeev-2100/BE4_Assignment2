41e4bf82fc734e5c137b09c7397e24881942eb29
Rajeev Rawat <rajeevrawat2100@gmail.com>
2025-11-25 14:36:15 +0530

    feat: add API endpoint to fetch all Data

--- index.js
+++ index.js
@@ -31,6 +31,26 @@ app.post('/recipes', async (req, res) => {
 })
 
 
+async function readAllRecipeDetail (){
+    try {
+        const recipe = await Recipe.find()
+        return recipe
+    } catch (error) {
+        throw error
+    }
+}
+
+app.get('/recipes', async (req,res) => {
+    try {
+        const recipe = await readAllRecipeDetail()
+        res.json(recipe)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).json({error: 'Failed to fetch recipe details'})
+    }
+})
+
+
 const PORT = 3000
 app.listen(PORT, () => {
     console.log('Server is running on this ', PORT)
